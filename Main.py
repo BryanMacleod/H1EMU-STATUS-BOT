dcf52153aaf10f93cdb5bf65a969e2b6169e3014
BryanMacleod <74321930+BryanMacleod@users.noreply.github.com>
2023-02-20 00:23:05 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,30 @@
+import discord
+
+# Initialize the bot
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Function to update status
+async def update_status():
+    while True:
+        try:
+            page = requests.get("https://serverlist.h1emu.com/us/")
+            soup = BeautifulSoup(page.content, 'html.parser')
+            table = soup.find_all('table')[0]
+            rows = table.find_all('tr')
+            for row in rows:
+                cols = row.find_all('td')
+                cols = [col.text.strip() for col in cols]
+                if len(cols) >= 2 and cols[1] == 'SERVERNAME':
+                    status = cols[3]
+                    game = discord.Game(name=f"{status} online in SERVERNAME")
+                    await bot.change_presence(activity=game)
+                    break
+        except:
+            pass
+        await asyncio.sleep(30)
+
+
+# Run the bot
+if __name__ == '__main__':
+    bot.run("TOKEN_HERE")
